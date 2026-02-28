4fb1995e6e8d0568ad0549dc4a320e2da0ecc8fb
MatheusSMA <matheusjut@gmail.com>
2026-02-28 12:03:27 -0300

    initial python tests

---
+++ game/breakout/constants.py
@@ -0,0 +1,40 @@
+# tamanho da janela
+LARGURA  = 800
+ALTURA   = 600
+
+# cores
+PRETO    = (  0,   0,   0)
+BRANCO   = (255, 255, 255)
+CIANO    = (  0, 255, 255)
+MAGENTA  = (255,   0, 255)
+AMARELO  = (255, 220,   0)
+VERMELHO = (200,  30,  30)
+
+# TODO: testar se 60fps aguenta bem dentro do Ren'Py
+FPS = 60
+
+# bola
+BOLA_RAIO  = 8
+BOLA_VEL_X = 4
+BOLA_VEL_Y = -4  # negativo = subindo
+
+# raquete
+RAQUETE_LARGURA = 100
+RAQUETE_ALTURA  = 12
+RAQUETE_VEL     = 6
+RAQUETE_Y       = ALTURA - 50
+
+# blocos
+BLOCO_LINHAS   = 5
+BLOCO_COLUNAS  = 10
+BLOCO_LARGURA  = 68
+BLOCO_ALTURA   = 22
+BLOCO_ESPACO_X = 5
+BLOCO_ESPACO_Y = 5
+BLOCO_OFFSET_X = 26
+BLOCO_OFFSET_Y = 60
+
+BLOCO_PONTOS = 10
+
+# TODO: adicionar cor por linha quando criar block.py
+# TODO: talvez aumentar velocidade da bola conforme pontuação sobe
